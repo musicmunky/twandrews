cc47fd0012f596da5aa12b1bb1ef259f923d1d67
musicmunky <musicmunky@gmail.com>
2015-07-28 18:09:46 -0400

    Initial commit for socrata code challenge

---
+++ codechallenge/index.php
@@ -0,0 +1,93 @@
+<?php
+	require_once("socrata.php");
+
+	$title = "CrimeWatch";
+
+	$view_uid = "3k2p-39jp";
+	$root_url = "https://data.seattle.gov";
+	$app_token = "rO91a2ol0Bibnga9u74y0VFNc";
+	$response = NULL;
+
+	$latitude = array_get("latitude", $_POST);
+	$longitude = array_get("longitude", $_POST);
+	$range = array_get("range", $_POST);
+
+	if($latitude != NULL && $longitude != NULL && $range != NULL) {
+		// Create a new unauthenticated client
+		$socrata = new Socrata($root_url, $app_token);
+
+		$params = array("\$where" => "within_circle(location, $latitude, $longitude, $range)");
+
+		$response = $socrata->get("/resource/$view_uid.json", $params);
+	}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11-strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+		<title><?php echo $title; ?></title>
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<link rel='stylesheet' type="text/css" href='css/ccstyle.css'  media="screen" charset="utf-8">
+		<link rel='stylesheet' type="text/css" href='css/fusionlib.css' media="screen" charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato">
+		<script language="javascript" type="text/javascript" src="javascript/jquery-1.11.3.min.js"></script>
+		<script language="javascript" type="text/javascript" src="javascript/fusionlib.js"></script>
+		<script language="javascript" type="text/javascript" src="https://www.google.com/jsapi"></script>
+		<script language="javascript" type="text/javascript" src="javascript/challenge.js"></script>
+	</head>
+	<body>
+		<!-- default to NYC if there's no localStorage -->
+		<input type="hidden" id="defaultzipcode" value="10001" />
+		<div id="header" class="header">
+			<div id="headercont" class="header-content">
+				<div class="header-logo">
+					<div class="logowrapper">
+						<div class="title">
+							<img id="logo" src="images/handcuffs.png" class="logoimage" />
+							<div id="titlediv" class="titletext"><?php echo $title; ?></div>
+						</div>
+					</div>
+					<div class="h100fl">
+						<div id="datewrapper" class="h100fl">
+							<span id="date" class="headspan"></span>
+						</div>
+					</div>
+				</div>
+
+				<div class="header-search">
+					<div class="w100fl h100fl" id="srchcont">
+						<span class="fl">Search: </span>
+						<form class="h100fl" id="searchform" onsubmit="runSearch();return false;">
+							<input type="text" id="searchbox" value="" class="searchbox"
+								   onkeyup="hideSearchDiv(this)" onchange="hideSearchDiv(this)" />
+							<button class="srchbtn">
+								<img src="images/magnify-glass.png" id="srchicn" />
+							</button>
+						</form>
+					</div>
+					<div id="locselect" class="locdiv"></div>
+				</div>
+			</div>
+		</div>
+<!--	<div id='displayinfodiv' style='width:100%;height:500px;margin-top:60px;overflow-y:scroll;'>
+			<pre><?php //var_dump(json_decode($content, true)); ?></pre>
+		</div> -->
+		<div id="mainwrapper" class="mainwrapper">
+		</div>
+		<div class="footer">
+			<div class="innerfooter tac">
+				<div class="footer-content-left">
+					<span>Crime info for <span id="footerlocation"></span></span>
+				</div>
+				<div class="footer-content-right">
+					<span>
+						Powered by <a href="http://dev.socrata.com/" class="cldclr" target="_blank">Socrata</a>
+						<span id="numreqs" style="color:#CCC"></span>
+					</span>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ codechallenge/php/geocode.php
@@ -0,0 +1,333 @@
+<?php
+
+	class Geocode
+	{
+		private $apikey;
+		private $apiurl;
+		private $geodat;
+		private $status;
+		private $rescnt;
+		private $lookup = array(
+							"suburb"	=> "neighborhood",
+							"localname" => "colloquial_area",
+							"city" 		=> "locality",
+							"state"		=> "administrative_area_level_1",
+							"county"	=> "administrative_area_level_2",
+							"district"	=> "administrative_area_level_3",
+							"aal4"		=> "administrative_area_level_4",
+							"aal5"		=> "administrative_area_level_5",
+							"country"	=> "country",
+							"zipcode"	=> "postal_code"
+						);
+/*
+    street_address => indicates a precise street address.
+
+    route => indicates a named route (such as "US 101").
+
+	intersection => indicates a major intersection, usually of two major roads.
+
+	political => indicates a political entity. Usually, this type indicates a polygon of some civil administration.
+
+	country => indicates the national political entity, and is typically the highest order type returned by the Geocoder.
+
+	administrative_area_level_1 => indicates a first-order civil entity below the country level. Within the United States, these administrative levels are states. Not all nations exhibit these administrative levels.
+
+	administrative_area_level_2 => indicates a second-order civil entity below the country level. Within the United States, these administrative levels are counties. Not all nations exhibit these administrative levels.
+
+	administrative_area_level_3 => indicates a third-order civil entity below the country level. This type indicates a minor civil division. Not all nations exhibit these administrative levels.
+
+	administrative_area_level_4 => indicates a fourth-order civil entity below the country level. This type indicates a minor civil division. Not all nations exhibit these administrative levels.
+
+	administrative_area_level_5 => indicates a fifth-order civil entity below the country level. This type indicates a minor civil division. Not all nations exhibit these administrative levels.
+
+	colloquial_area => indicates a commonly-used alternative name for the entity.
+
+	locality => indicates an incorporated city or town political entity.
+
+	ward => indicates a specific type of Japanese locality, to facilitate distinction between multiple locality components within a Japanese address.
+
+	sublocality => indicates a first-order civil entity below a locality. For some locations may receive one of the additional types: sublocality_level_1 to sublocality_level_5. Each sublocality level is a civil entity. Larger numbers indicate a smaller geographic area.
+
+	neighborhood => indicates a named neighborhood
+
+	premise => indicates a named location, usually a building or collection of buildings with a common name
+
+	subpremise => indicates a first-order entity below a named location, usually a singular building within a collection of buildings with a common name
+
+	postal_code => indicates a postal code as used to address postal mail within the country.
+
+	natural_feature => indicates a prominent natural feature.
+
+	airport => indicates an airport.
+
+	park => indicates a named park.
+
+	point_of_interest => indicates a named point of interest. Typically, these "POI"s are prominent local entities that don't easily fit in another category, such as "Empire State Building" or "Statue of Liberty."
+
+*/
+
+		/**
+		* Create a new instance
+		*
+		* @param Boolean $loadfromdb
+		* @param String $apikey
+		* @param String $apiurl
+		*/
+		function __construct($loadfromdb, $apikey = "", $apiurl = "")
+		{
+			if($loadfromdb)
+			{
+				try {
+					$apinfo = mysql_fetch_assoc(mysql_query("SELECT APIKEY, URL FROM weatherapikeys WHERE SERVICE='google';"));
+					$apikey = $apinfo['APIKEY'];
+					$apiurl = $apinfo['URL'];
+				}
+				catch(Exception $e) {}
+
+			}
+			$this->apikey = $apikey;
+			$this->apiurl = $apiurl;
+		}
+
+		public function getApiKey()
+		{
+			return $this->apikey;
+		}
+
+		public function setApiKey($key)
+		{
+			$r = true;
+			try {
+				$this->apikey = $key;
+			}
+			catch(Exception $e) {
+				$r = false;
+			}
+			return $r;
+		}
+
+		public function getApiUrl()
+		{
+			return $this->apiurl;
+		}
+
+		public function setApiUrl($url)
+		{
+			$r = true;
+			try {
+				$this->apiurl = $url;
+			}
+			catch(Exception $e) {
+				$r = false;
+			}
+			return $r;
+		}
+
+		public function getLatitude($type = "string")
+		{
+			return $this->getLatLongInformation("lat", $type);
+		}
+
+		public function getLongitude($type = "string")
+		{
+			return $this->getLatLongInformation("lng", $type);
+		}
+
+		public function getLatLong($type = "1DArray")
+		{
+			return $this->getLatLongInformation("latlng", $type);
+		}
+
+		public function getFormattedAddress($type = "string")
+		{
+			return $this->getPlaceInformation("formatted_address", $type);
+		}
+
+		public function getPlaceID($type = "string")
+		{
+			return $this->getPlaceInformation("place_id", $type);
+		}
+
+		public function getSuburb($type = "string")
+		{
+			return $this->getAddressComponents("suburb", $type);
+		}
+
+		public function getCity($type = "string")
+		{
+			return $this->getAddressComponents("city", $type);
+		}
+
+		public function getState($type = "string")
+		{
+			return $this->getAddressComponents("state", $type);
+		}
+
+		public function getCountry($type = "string")
+		{
+			return $this->getAddressComponents("country", $type);
+		}
+
+		public function getZipCode($type = "string")
+		{
+			return $this->getAddressComponents("zipcode", $type);
+		}
+
+		public function getResultCount()
+		{
+			return $this->rescnt;
+		}
+
+		public function getStatus()
+		{
+			return $this->status;
+		}
+
+		public function getGeoData()
+		{
+			return $this->geodat;
+		}
+
+		public function loadGeoData($srch)
+		{
+			try {
+				$url = $this->getApiUrl();
+				$key = $this->getApiKey();
+				if($srch == "")
+				{
+					throw new Exception("Invalid search string");
+				}
+
+				if($url == "" || $key == "")
+				{
+					throw new Exception("Invalid parameters set for Geocode object - please check value of Key and Url");
+				}
+
+				$search   = urlencode($srch);
+				$requrl   = $url . "address=" . $search . "&key=" . $key;
+				$content  = file_get_contents($requrl);
+				$rescon   = json_decode($content, true);
+
+				if(!isset($rescon['status']) || $rescon['status'] != "OK")
+				{
+					throw new Exception("Bad result from geocode server - please check parameters and try again");
+				}
+
+				$this->rescnt = count($rescon['results']);
+				$this->geodat = $rescon['results'];
+				$this->status = $rescon['status'];
+			}
+			catch(Exception $e){
+				$this->status = "ERROR: " . $e->getMessage();
+				$this->rescnt = -1;
+				$this->geodat = array("ERROR" => $e->getMessage());
+			}
+		}
+
+		private function getLatLongInformation($k, $type)
+		{
+			$rval = array();
+			$rslt = $this->getGeoData();
+			$alen = count($rslt);
+			try {
+				if($k == "latlng"){
+					if($type == "array" || $type == "a"){
+						for($i = 0; $i < $alen; $i++)
+						{
+							array_push($rval, array($rslt[$i]['geometry']['location']['lat'], $rslt[$i]['geometry']['location']['lng']));
+						}
+					}
+					else
+					{
+						array_push($rval, array($rslt[0]['geometry']['location']['lat'], $rslt[0]['geometry']['location']['lng']));
+					}
+				}
+				else
+				{
+					if($type == "array" || $type == "a"){
+						for($i = 0; $i < $alen; $i++)
+						{
+							array_push($rval, $rslt[$i]['geometry']['location'][$k]);
+						}
+					}
+					else
+					{
+						$rval = $rslt[0]['geometry']['location'][$k];
+					}
+				}
+
+			} catch(Exception $e) {}
+			return $rval;
+		}
+
+		private function getPlaceInformation($k, $type)
+		{
+			$rval = array();
+			$kval = $k;
+			$rslt = $this->getGeoData();
+			$alen = count($rslt);
+			try {
+				if($type == "array" || $type == "a"){
+					for($i = 0; $i < $alen; $i++)
+					{
+						array_push($rval, $rslt[$i][$kval]);
+					}
+				}
+				else
+				{
+					$rval = $rslt[0][$kval];
+				}
+			} catch(Exception $e) { $rval = $this->status; }
+			return $rval;
+		}
+
+		private function getAddressComponents($k, $type)
+		{
+			$rval = array();
+			$kval = $this->lookup[$k];
+			$rslt = $this->getGeoData();
+			$alen = count($rslt);
+			try {
+				if($type == "array" || $type == "a"){
+					for($i = 0; $i < $alen; $i++)
+					{
+						$addcomps = $rslt[$i]['address_components'];
+						$flag = 0;
+						for($j = 0; $j < count($addcomps); $j++)
+						{
+							if($addcomps[$j]['types'][0] == $kval)
+							{
+								array_push($rval, array("long_name" => $addcomps[$j]['long_name'], "short_name" => $addcomps[$j]['short_name']));
+								$flag = 1;
+								break;
+							}
+						}
+						if($flag == 0){
+							array_push($rval, array("long_name" => "NO_DATA_FOR_KEY " . $kval, "short_name" => "NO_DATA"));
+						}
+					}
+				}
+				else
+				{
+					$addcomps = $rslt[0]['address_components'];
+					$flag = 0;
+					for($j = 0; $j < count($addcomps); $j++)
+					{
+						if($addcomps[$j]['types'][0] == $kval)
+						{
+							$rval = ($kval == "locality") ? $addcomps[$j]['long_name'] : $addcomps[$j]['short_name'];
+							$flag = 1;
+							break;
+						}
+					}
+					if($flag == 0){
+						$rval = "NO_DATA";
+					}
+				}
+			} catch(Exception $e) { $rval = $this->status; }
+			return $rval;
+		}
+
+	}
+
+?>
