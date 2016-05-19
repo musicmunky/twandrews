ecbacfb72e969d275b35925c820bd4dc68cd4485
musicmunky <musicmunky@gmail.com>
2016-05-19 15:56:05 -0400

    Update with 'oops' page, gitignore, and removing nodeproject directory

---
+++ oops.php
@@ -0,0 +1,27 @@
+<?php ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11-strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Oops!</title>
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<link rel='stylesheet' href='css/bootstrap.css' type="text/css" media="screen" charset="utf-8">
+	</head>
+	<body>
+		<div id="maincontainer" class="container">
+			<h2 style="text-align:center;">OOPS!</h2>
+			<div style="width:500px;margin-left:auto;margin-right:auto;">
+				<p>Hey there, friend!  It looks like you've stumbled onto this page because you've sent a few too many requests to my server!</p>
+				<p>There could be any number of reasons for this - aliens, your cat learned to use your computer, the CIA...who knows??</p>
+				<p>The point is, I'd like to know a little more about you!  Why, you ask?  Well see, this is a private server and I use it for my personal projects.</p>
+				<p>It's kept up and running out of my own pocket, and if certain things get out of hand (bandwidth, API calls, etc) it actually costs me real money.</p>
+				<p>I'm sure you're a nice person though, so please don't be offended or insulted!  If you had a legitimate reason for making requests to my server, please feel free to let me know!</p>
+				<p>If you want to contact me you can find my email address on my <a href="https://github.com/musicmunky" target="_blank">GitHub page</a>.</p>
+				<p>Feel free to drop me a line and let me know how I can help you out!</p>
+				<p>All the best,</p>
+				<p>tim</p>
+			</div>
+		</div>
+	</body>
+</html>
