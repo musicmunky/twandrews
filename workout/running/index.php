7b738dcaa11bcf087376a7620a43384e44b5c411
tandrews <tandrews@andrewsdo.(none)>
2018-01-26 07:33:04 -0500

    adding the last couple of runs and a few styling changes

--- workout/running/index.php
+++ workout/running/index.php
@@ -4,6 +4,7 @@
     <head>
         <title>Training Schedule</title>
 		<link rel="icon" type="image/icon" href="images/favicon.ico">
+		<link rel='stylesheet' href='../../css/bootstrap.css' type="text/css" media="screen" charset="utf-8">
 		<link rel='stylesheet' href='css/running.css' type="text/css" media="screen" charset="utf-8">
 		<link rel='stylesheet' href='css/fusionlib.css' type="text/css" media="screen" charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:100,200,300,400,500,600,700,800,900">
@@ -12,48 +13,75 @@
 		<script language="javascript" type="text/javascript" src="js/workout.js"></script>
     </head>
     <body>
-        <div id="main_wrapper" class="main_wrapper">
+        <div id="main_wrapper" class="container main_wrapper">
             <div class="heading_wrapper">
                 Half Marathon Training Program
             </div>
             <div id="week_01" class="week_wrapper">
-                <div class="week_list">Week 01 <span class="week_span">Jan 15 - Jan 20</span></div>
+                <div class="week_list">Week 01 <span class="week_span">Jan 22 - Jan 27</span></div>
                 <div id="training_01" class="training_wrapper">
                     <ul><li>Monday - 1st Workout: Fartleks 10 min warmup, 10 x (1 min @ 6:30 / 1 min jog), 10 min cooldown</li>
                         <ul>
-                            <li>5.7 miles <a href="https://www.strava.com/activities/1358345049"><i>(View on Strava)</i></a></li>
+                            <li>5.2 miles <a target="_blank" class="btn btn-primary btn-xs" href="https://www.strava.com/activities/1369554166">View on Strava</a></li>
                         </ul>
-                        <li>Tuesday - Normal Run (figure out mileage)</li>
-                        <li>Wednesday - Normal Run (figure out mileage)</li>
-                        <li>Thurday - Normal Run (figure out mileage)</li>
+                        <li>Tuesday - Normal Run</li>
+                        <ul>
+                            <li>5.2 miles <a target="_blank" class="btn btn-primary btn-xs" href="https://www.strava.com/activities/1371119275">View on Strava</a></li>
+                        </ul>
+                        <li>Wednesday - Normal Run</li>
+                        <ul>
+                            <li>5.1 miles <a target="_blank" class="btn btn-primary btn-xs" href="https://www.strava.com/activities/1372814410">View on Strava</a></li>
+                        </ul>
+                        <li>Thurday - Normal Run</li>
+                        <ul>
+                            <li>5.2 miles <a target="_blank" class="btn btn-primary btn-xs" href="https://www.strava.com/activities/1374417945">View on Strava</a></li>
+                        </ul>
                         <li>Friday - 2nd Workout: 10 min warmup, 2 mile tempo @ 13:15 - 13:30, 10 min cool down</li>
+                        <ul>
+                            <li>4.6 miles <a target="_blank" class="btn btn-primary btn-xs" href="https://www.strava.com/activities/1375963789">View on Strava</a></li>
+                        </ul>
                         <li>Saturday - 3rd Workout: Long run 7 miles EASY (7:45 or so)</li>
+                        <ul>
+                            <li>7 miles</li>
+                        </ul>
                         <li>Sunday - Rest day</li>
                     </ul>
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>32 miles
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span> let me know how easy/hard Friday's work out is so I can gauge your pace.  Fill in the rest of the mileage until you get to 27-30 miles for the week.
                 </div>
             </div>
             <div id="week_02" class="week_wrapper">
-                <div class="week_list">Week 02 <span class="week_span">Jan 22 - Jan 27</span></div>
+                <div class="week_list">Week 02 <span class="week_span">Jan 29 - Feb 3</span></div>
                 <div id="training_02" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_03" class="week_wrapper">
-                <div class="week_list">Week 03 <span class="week_span">Jan 29 - Feb 3</span></div>
+                <div class="week_list">Week 03 <span class="week_span">Feb 5 - Feb 10</span></div>
                 <div id="training_03" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_04" class="week_wrapper">
-                <div class="week_list">Week 04 <span class="week_span">Feb 5 - Feb 10</span></div>
+                <div class="week_list">Week 04 <span class="week_span">Feb 12 - Feb 17</span></div>
                 <div id="training_04" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
@@ -63,101 +91,129 @@
                 <div class="week_list">Week 05 <span class="week_span">Feb 12 - Feb 17</span></div>
                 <div id="training_05" class="training_wrapper">
                 </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
+                </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_06" class="week_wrapper">
-                <div class="week_list">Week 06 <span class="week_span">Feb 19 - Feb 24</span></div>
+                <div class="week_list">Week 06 <span class="week_span">Feb 26 - Mar 3</span></div>
                 <div id="training_06" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_07" class="week_wrapper">
-                <div class="week_list">Week 07 <span class="week_span">Feb 26 - Mar 3</span></div>
+                <div class="week_list">Week 07 <span class="week_span">Mar 5 - Mar 10</span></div>
                 <div id="training_07" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_08" class="week_wrapper">
-                <div class="week_list">Week 08 <span class="week_span">Mar 5 - Mar 10</span></div>
+                <div class="week_list">Week 08 <span class="week_span">Mar 12 - Mar 17</span></div>
                 <div id="training_08" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_09" class="week_wrapper">
-                <div class="week_list">Week 09 <span class="week_span">Mar 12 - Mar 17</span></div>
+                <div class="week_list">Week 09 <span class="week_span">Mar 19 - Mar 24</span></div>
                 <div id="training_09" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_10" class="week_wrapper">
-                <div class="week_list">Week 10 <span class="week_span">Mar 19 - Mar 24</span></div>
+                <div class="week_list">Week 10 <span class="week_span">Mar 26 - Mar 31</span></div>
                 <div id="training_10" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_11" class="week_wrapper">
-                <div class="week_list">Week 11 <span class="week_span">Mar 26 - Mar 31</span></div>
+                <div class="week_list">Week 11 <span class="week_span">Apr 2 - Apr 7</span></div>
                 <div id="training_11" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_12" class="week_wrapper">
-                <div class="week_list">Week 12 <span class="week_span">Apr 2 - Apr 7</span></div>
+                <div class="week_list">Week 12 <span class="week_span">Apr 9 - Apr 14</span></div>
                 <div id="training_12" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_13" class="week_wrapper">
-                <div class="week_list">Week 13 <span class="week_span">Apr 9 - Apr 14</span></div>
+                <div class="week_list">Week 13 <span class="week_span">Apr 16 - Apr 21</span></div>
                 <div id="training_13" class="training_wrapper">
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
+                </div>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_14" class="week_wrapper">
-                <div class="week_list">Week 14 <span class="week_span">Apr 16 - Apr 21</span></div>
+                <div class="week_list">Week 14 <span class="week_span">Apr 23 - Apr 28</span></div>
                 <div id="training_14" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_15" class="week_wrapper">
-                <div class="week_list">Week 15 <span class="week_span">Apr 23 - Apr 28</span></div>
+                <div class="week_list">Week 15 <span class="week_span">Apr 30 - May 5</span></div>
                 <div id="training_15" class="training_wrapper">
+                </div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>
                 </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
             </div>
             <div id="week_16" class="week_wrapper">
-                <div class="week_list">Week 16 <span class="week_span">Apr 30 - May 5</span></div>
-                <div id="training_16" class="training_wrapper">
-                </div>
-                <div class="notes">
-                    <span class="note_span">Note:</span>
-                </div>
-            </div>
-            <div id="week_17" class="week_wrapper">
                 <div class="week_list">May 6 <span class="week_span">RACE DAY!</span></div>
-                <div id="training_16" class="training_wrapper"></div>
+                <div id="training_17" class="training_wrapper"></div>
+                <div class="total_mileage">
+                    <span class="note_span">Total Mileage: </span>13.1 miles
+                </div>
                 <div class="notes">
                     <span class="note_span">Note:</span>
                 </div>
